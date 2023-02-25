237f72b08ba159950873af8b81291916a2f7a39b
NazimovGIT <nazimmov@ya.ru>
2023-02-25 14:42:28 +0300

    commit

--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -3,6 +3,9 @@ package org.example;
 import org.springframework.stereotype.Component;
 
 public class RockMusic implements Music{
+    public RockMusic() {
+    }
+
     @Override
     public String getSong() {
         return "Песня из рок музыки";
